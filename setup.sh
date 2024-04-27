1057e602c499adc342df0b380d918e5cf50b26d5
LoBrol <andreabrollo047@gmail.com>
2024-04-27 21:29:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,8 +26,8 @@ sudo apt install -y openssh-server nano ufw curl wget git unzip zsh neofetch lm-
 
 
 # --- Allow SSH on FIREWALL ---
-sudo ufw default deny incoming
-sudo ufw default deny outgoing
+#sudo ufw default deny incoming
+#sudo ufw default deny outgoing
 sudo ufw allow 22/tcp
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp

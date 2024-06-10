9d4146b811f200821461bbae22bbc1ace6d9d134
LoBrol <andreabrollo047@gmail.com>
2024-06-10 18:30:43 +0200

    Add files via upload

---
+++ file_to_be_copied/config.php
@@ -0,0 +1,34 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocu44oxn1ucm',
+  'passwordsalt' => 'BQYOXJ1vvLeQB3n9/2YZjnRQ3kTFJ+',
+  'secret' => '+7aTaMLr4wr5+wBSedufSITHXIiWMFcKQjxsy8QfCQ2KpAoT',
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.local',
+    1 => 'dommymommy.duckdns.org',
+  ),
+  'datadirectory' => '/mnt/NEXTCLOUD_DATA',
+  'dbtype' => 'mysql',
+  'version' => '29.0.2.2',
+  'overwrite.cli.url' => 'http://nextcloud.local',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+  'maintenance' => false,
+  'loglevel' => 0,
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memachec.locking' => '\\OC\\Memcache\\Redis',
+  'redis' => 
+  array (
+    'host' => 'localhost',
+    'port' => 6379,
+    'timeout' => 1,
+    'password' => '',
+  ),
+);
